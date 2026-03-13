a615c9a32505720ce0727ab25ef33fafb21eb430
Valerii <valerabokoc305@gmail.com>
2026-03-13 15:25:52 +0200

    feature: added facts section

--- wp-content/themes/igrmed/pages/about.php
+++ wp-content/themes/igrmed/pages/about.php
@@ -8,6 +8,7 @@ Template Name: About
 
 <main id="about">
     <?php get_template_part('sections/about/hero'); ?>
+    <?php get_template_part('sections/about/facts'); ?>
 </main>
 
 <?php get_footer(); ?>
