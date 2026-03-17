fe413894a3c8af9b25e9efeae2f9eaf5d757407d
Valerii <valerabokoc305@gmail.com>
2026-03-17 14:11:35 +0200

    feature: added advantages section

---
+++ wp-content/themes/igrmed/assets/js/swipers/infertility-women/main.js
@@ -0,0 +1,1 @@
+import "./advantages-preg-swiper";
--- wp-content/themes/igrmed/single-infertility-women.php
+++ wp-content/themes/igrmed/single-infertility-women.php
@@ -11,6 +11,8 @@ get_header();
 
     <?php get_template_part('sections/infertility-women/hero'); ?>
     <?php get_template_part('sections/infertility-women/what-examinations'); ?>
+    <?php get_template_part('sections/infertility-women/advantages-preg'); ?>
+    <?php get_template_part('sections/home/cta'); ?>
 
 
 </main>
