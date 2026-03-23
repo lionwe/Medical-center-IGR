5384443f16e85e0b74524793d19739393b1206e0
lionwe <lionwe433@gmail.com>
2026-03-23 14:17:00 +0200

    refactor: unify simple-banner styles and implement adaptive backgrounds

--- wp-content/themes/igrmed/sections/price/hero.php
+++ wp-content/themes/igrmed/sections/price/hero.php
@@ -1,16 +1,12 @@
 <?php
-
 /**
  * Price Page Hero Section
  *
  * @package IGRMed
  */
-
-$hero_bg_url = get_template_directory_uri() . '/assets/img/bread-crumbs.webp';
-$hero_style = ' style="background-image: url(' . esc_url($hero_bg_url) . ');"';
 ?>
 
-<section class="price-hero" <?php echo $hero_style; ?>>
+<section class="price-hero simple-banner-section">
     <div class="container">
         <?php get_template_part('templates/breadcrumbs'); ?>
         <?php
--- wp-content/themes/igrmed/sections/privacy-policy/hero.php
+++ wp-content/themes/igrmed/sections/privacy-policy/hero.php
@@ -1,16 +1,12 @@
 <?php
-
 /**
  * Privacy Policy Hero Section
  *
  * @package IGRMed
  */
-
-$hero_bg_url = get_template_directory_uri() . '/assets/img/bread-crumbs.webp';
-$hero_style = ' style="background-image: url(' . esc_url($hero_bg_url) . ');"';
 ?>
 
-<section class="privacy-hero" <?php echo $hero_style; ?>>
+<section class="privacy-hero simple-banner-section">
     <div class="container">
         <?php get_template_part('templates/breadcrumbs'); ?>
         <div class="simple-banner">
