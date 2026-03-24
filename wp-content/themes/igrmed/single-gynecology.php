a8527f6ac01d494b817cf75af0b88d4118356993
Valerii <valerabokoc305@gmail.com>
2026-03-24 18:26:44 +0200

    fixed: overflow banner

--- wp-content/themes/igrmed/single-gynecology.php
+++ wp-content/themes/igrmed/single-gynecology.php
@@ -10,7 +10,6 @@ get_header();
 <main id="gynecology">
     <?php get_template_part('sections/gynecology/hero'); ?>
     <?php get_template_part('sections/gynecology/content'); ?>
-    <?php get_template_part('templates/overflow-banner'); ?>
     <?php get_template_part('sections/services/faq'); ?>
     <?php get_template_part('sections/home/cta'); ?>
 </main>
--- wp-content/themes/igrmed/single-male-infertility.php
+++ wp-content/themes/igrmed/single-male-infertility.php
@@ -10,7 +10,6 @@ get_header();
 <main id="male-infertility">
     <?php get_template_part('sections/male-infertility/hero'); ?>
     <?php get_template_part('sections/male-infertility/content'); ?>
-    <?php get_template_part('templates/overflow-banner'); ?>
     <?php get_template_part('sections/services/faq'); ?>
     <?php get_template_part('sections/home/cta'); ?>
 </main>
