ed5a3b0a8046499d4c1ba7612095ac793b46e9df
Valerii <valerabokoc305@gmail.com>
2026-03-26 10:20:20 +0200

    fix: changed place of banner

--- wp-content/themes/igrmed/single-egg-donor.php
+++ wp-content/themes/igrmed/single-egg-donor.php
@@ -10,7 +10,6 @@ get_header();
 <main id="egg-donor">
     <?php get_template_part('sections/egg-donor/hero'); ?>
     <?php get_template_part('sections/egg-donor/content'); ?>
-    <?php get_template_part('templates/overflow-banner'); ?>
     <?php get_template_part('sections/services/faq'); ?>
     <?php get_template_part('sections/home/cta'); ?>
 </main>
