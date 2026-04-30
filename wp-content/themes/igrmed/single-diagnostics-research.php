3a8f94d15b993613899d8bdba305edc3056d20a8
Valerii <valerabokoc305@gmail.com>
2026-04-30 12:29:04 +0300

    Add CTA section to diagnostics page

--- wp-content/themes/igrmed/single-diagnostics-research.php
+++ wp-content/themes/igrmed/single-diagnostics-research.php
@@ -11,6 +11,7 @@ get_header();
     <?php get_template_part('sections/diagnostics-research/hero'); ?>
     <?php get_template_part('sections/diagnostics-research/diagnostics'); ?>
     <?php get_template_part('sections/services/faq'); ?>
+    <?php get_template_part('sections/home/cta'); ?>
 </main>
 
 <?php get_footer('simple'); ?>
